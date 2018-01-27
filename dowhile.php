754b7ada2f0fb784fef446b4c22d0446e0d4cf77
Tharindu_Dhananjaya <tharindudhananjaya1122@gmail.com>
2018-01-27 13:44:00 +0530

    Add files via upload

---
+++ dowhile.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	// Do whele
+	$x=1;
+		do {
+			echo "true<br>";
+			$x++;
+		}
+		while ($x < 5) ;
+				 
+	echo "<br>";
+	?>	
+</body>
+</html>
