f315b218d897e57a93eea3027461d3b7570b356a
Tharindu_Dhananjaya <tharindudhananjaya1122@gmail.com>
2018-01-28 01:06:14 +0530

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	// for each
+	$array = array("Daniel","Jane","Jacob" );
+				
+				foreach ($array as $loopdata) {
+					echo "My name is ".$loopdata."<br>";
+				}
+	?>	
+</body>
+</html>
